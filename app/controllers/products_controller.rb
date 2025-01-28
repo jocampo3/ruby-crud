10760b27ce3647c0bd4ad0b6169a71d23f8020b5
Jose Ocampo <ocampojose305@gmail.com>
2025-01-28 17:53:47 -0600

    pushing code to github for public view

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,44 @@
+class ProductsController < ApplicationController
+  before_action :set_product, only: [:update, :delete]
+  
+  def index
+    @products = Product.all
+    @product = Product.new
+  end
+  
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path, notice: "Product Created Successfully"
+    else
+      render :index, notice: "Product could not be created"
+    end
+  end
+  
+  def update
+    if @product.update(product_params)
+      redirect_to products_path, notice: "Product update successfully"
+    else
+      render :index, notice: "Product could not be updated"
+    end
+  end
+  
+  def delete
+    if @product.destroy
+      redirect_to products_path, notice: "Product deleted successfully"
+    else
+      render :index, notice: "Failed to delete product"
+    end
+  end
+
+  private
+  
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name)
+  end
+
+end
